455d636f5aaf3dd4012d7d6a506f3be0f555ab07
3dw1nM0535 <workockmoses@gmail.com>
2018-01-11 22:07:00 +0300

    Articles view

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 // Init express module
 const app = express();
@@ -6,9 +7,18 @@ const app = express();
 // Init port number for server
 const port = 3000 || process.env.PORT;
 
+// views Middleware
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // Home route handler
 app.get('/', function (req, res) {
-	res.send('Hello, worold');
+	res.render('index');
+});
+
+// Add articles route handler
+app.get('/articles/add', function (req, res) {
+	res.render('addArticle');
 });
 
 // Init server on port 3000 || process.env.PORT
